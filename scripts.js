66b5aee5c1d6a4f0a2c44f0e514ed7311e2b9604
tatemange <utatem3@gmail.com>
2025-04-01 20:31:58 +0100

    new commit v01

--- scripts.js
+++ scripts.js
@@ -1,7 +1,11 @@
 
+// const OPERATEUR_AND_SIGNE = "÷ C × ±  − % . = +"
 
-const KEYS = "7 8 9 ÷ C 4 5 6 × ± 1 2 3 − % 0 . = +"
-let FN_KEYS = "cos sin tan √ ln log π abs mod ( ) !"
+const KEYS = "( ) mc m+ m- mr RM +/- % ÷ 2^nd x^2 x^3 x^y " +
+    "e^x 10^x 7 8 9 × 1/x 2Rx 3Rx yRx ln log_10 4 5 6 - " +
+    "x! sin cos tan e EE 1 2 3 + s sinh cosh tanh pi " +
+    "Rad Rand 0 , ="
+
 
 
 function createHtmlElt(tagName, attrs={}) {
@@ -27,17 +31,41 @@ function insertButtons(string, parent){
         let button = createHtmlElt('button', {text: str, 'class': `b${str} ${cls} btn`})
         parent.appendChild(button)
     })
+
+    setStyle([...parent.children], 10, 9)
 }
 
-insertButtons(FN_KEYS, document.querySelector(".buttons"))
 insertButtons(KEYS, document.querySelector(".buttons"))
 
-class Calculator {
-    /**
-     * @param {HTMLElement} calc
-     */
-    constructor(calc) {
-        this.calc = calc;
-    }
-
+/**
+ * 
+ * @param {Array<HTMLElement>} array 
+ * @param {Number} nbCol 
+ * @param {Number} moins 
+ */
+function setStyle(array, nbCol, moins) {
+    array.forEach((btn, i) => {
+        if((i % nbCol)-moins === 0) 
+            btn.classList.add("operator")
+        for(let j = 1;j < 4; j++){
+            if((i % nbCol)-moins+j === 0) {
+                btn.classList.add("number")
+                if(!["RM", "+/-", "Rand", "%"].includes(btn.textContent))
+                    btn.classList.add('font-size-xxl')
+                else{
+                    if(btn.textContent  !== "Rand")
+                        btn.classList.add('up-btn')
+                    else btn.classList.add("rand")
+                }
+            }
+        }
+    })
 }
+
+
+function modified(button) {
+    for(let i = 0; i<button.textContent.length; i++) {
+
+    }
+}
+
